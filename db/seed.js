26fe864cb86ebbf42bbb76362b16af152253a307
Nathan Martel <nathanmartel@gmail.com>
2020-03-27 11:18:45 -0700

    Added actor model, routes, and tests

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,8 @@
 const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
 const chance = require('chance').Chance();
 
-module.exports = async({ studiosToCreate = 5 } = {}) => {
+module.exports = async({ studiosToCreate = 5, actorsToCreate = 5 } = {}) => {
   await Studio.create([...Array(studiosToCreate)].map(() => ({
     name: chance.company(),
     address: {
@@ -9,5 +10,11 @@ module.exports = async({ studiosToCreate = 5 } = {}) => {
       state: chance.state(),
       country: chance.country({ full: true }),
     }
+  })));
+
+  await Actor.create([...Array(actorsToCreate)].map(() => ({
+    name: chance.name(),
+    dob: chance.date(),
+    pob: chance.city() + ', ' + chance.state()
   })));
 };
