e92c07b0659dc6b9be199b1c4f549f6d6dc7547c
Nathan Martel <nathanmartel@gmail.com>
2020-03-30 18:29:23 -0700

    Review model, routes, and tests added

--- db/seed.js
+++ db/seed.js
@@ -4,9 +4,10 @@ const Studio = require('../lib/models/Studio');
 const Actor = require('../lib/models/Actor');
 const Reviewer = require('../lib/models/Reviewer');
 const Film = require('../lib/models/Film');
+const Review = require('../lib/models/Review');
 const chance = require('chance').Chance();
 
-module.exports = async({ studiosToCreate = 3, actorsToCreate = 5, reviewersToCreate = 7, filmsToCreate = 9 } = {}) => {
+module.exports = async({ studiosToCreate = 3, actorsToCreate = 5, reviewersToCreate = 7, filmsToCreate = 9, reviewsToCreate = 150 } = {}) => {
   const studios = await Studio.create([...Array(studiosToCreate)].map(() => ({
     name: chance.company(),
     address: {
@@ -22,12 +23,12 @@ module.exports = async({ studiosToCreate = 3, actorsToCreate = 5, reviewersToCre
     pob: chance.city() + ', ' + chance.state()
   })));
 
-  await Reviewer.create([...Array(reviewersToCreate)].map(() => ({
+  const reviewers = await Reviewer.create([...Array(reviewersToCreate)].map(() => ({
     name: chance.name(),
     company: chance.company()
   })));
 
-  await Film.create([...Array(filmsToCreate)].map(() => ({
+  const films = await Film.create([...Array(filmsToCreate)].map(() => ({
     title: chance.animal() + chance.animal(),
     studio: chance.pickone(studios)._id,
     released: chance.integer({ min: 1900, max: 2020 }),
@@ -35,5 +36,12 @@ module.exports = async({ studiosToCreate = 3, actorsToCreate = 5, reviewersToCre
       role: chance.name(),
       actor: chance.pickone(actors)._id
     }))
+  })));
+
+  await Review.create([...Array(reviewsToCreate)].map(() => ({
+    rating: chance.integer({ min: 1, max: 5 }),
+    reviewer: chance.pickone(reviewers)._id,
+    review: chance.sentence(),
+    film: chance.pickone(films)._id
   })));
 };
